3f674539b032a4070ed31ed89654c562359d05f6
Patrick Miranda <patrickrsmiranda@gmail.com>
2021-05-12 02:14:30 -0400

    [001] creating first files project

---
+++ app.js
@@ -0,0 +1,33 @@
+function sortValues(values) {
+  var listItem = null;
+  var sortedValues = null;
+  if (values[0] !== "") {
+    document.getElementById("valuesSorted").innerHTML = "";
+    sortedValues = values.reverse();
+    var listValues = document.getElementById("valuesSorted");
+    sortedValues
+      .sort(function (fValue, lValue) {
+        return lValue - fValue;
+      })
+      .forEach((element, index) => {
+        listItem = document.createElement("li");
+        listItem.innerHTML = element;
+        listValues.appendChild(listItem);
+      });
+  }
+}
+
+function getValues() {
+  var values = document
+    .getElementById("values")
+    .value.replace(/\s/g, "")
+    .split("-");
+  sortValues(values);
+}
+
+function clearValues(data) {
+  if (!!data.value && typeof data.value !== "" && data.value !== undefined) {
+    document.getElementById("valuesSorted").innerHTML = "";
+    //window.location.reload();
+  }
+}
